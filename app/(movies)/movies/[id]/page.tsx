41f1807e3524c8e33f2667f55f946054e20815dd
shka5 <shka5.dev@gmail.com>
2024-07-07 19:17:30 +0900

    2024-07-07 ENH Add - Suspense 추가

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,17 +1,17 @@
+import { Suspense } from "react"
 import { API_URL } from "../../../(home)/page"
-
-async function getMovie(id:string) {
-    const response = await fetch(`${API_URL}/${id}`);
-    return response.json();
-}
-
-async function getVideos(id:string) {
-    const response = await fetch(`${API_URL}/${id}/videos`);
-}
+import MovieInfo from "../../../../components/movie-info"
+import MovieVideos from "../../../../components/movie-videos"
 
 export default async function MoviesDetail({params:{id}}:{
     params:{id :string}
 }){
-    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)])
-    return <h1>{movie.title}</h1>
+    return <div>
+        <Suspense fallback={<h1>Lodaing movie info</h1>}>
+            <MovieInfo id={id} />
+        </Suspense>
+        <Suspense fallback={<h1>Lodaing movie Video</h1>}>
+            <MovieVideos id={id} />
+        </Suspense>
+    </div>
 }
