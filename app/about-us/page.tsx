3472ddc8b55a43b8a6439815782c9b9a27a7432b
shka5 <shka5.dev@gmail.com>
2024-07-07 13:14:53 +0900

    layout 추가

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -3,7 +3,6 @@ import Navigation from "../../components/navigation";
 export default function AboutUS(){
     return (
         <div>
-            <Navigation />
             <h1>About us!</h1>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Navigation from "../components/navigation";
 export default function Root(){
     return (
         <div>
-            <Navigation />
             <h1>Hello!</h1>
         </div>
-    );
+    )
 }
